766429f460867a031f4b3e2dfcd1ab87dc6bf1f9
Debian Live user <user@localhost.localdomain>
2015-09-04 01:07:27 -0400

    another commit

--- sudo-first.sh
+++ sudo-first.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#
+# Author: Art Lux
+# Copyright: Art Lux 2015
+# Info: Debian 8 live user customization script.
+
 if [ "$EUID" -ne 0 ]
   then echo "Are you root? "
   exit
@@ -9,37 +14,59 @@ fi
 apt-get -y remove ssh;
 
 #remove exim
+#/etc/init.d/exim4 stop;
+killall exim4;
 apt-get -y remove exim4;
 
 #remove avahi 
 killall avahi-daemon;
-apt-get -y remove avahi-daemon;
+#apt-get -y remove avahi-daemon;
 
 
 #create log directory
-mkdir /home/user/logs;
-chown -R user. /home/user/logs;
+#mkdir /home/user/logs;
+#chown -R user. /home/user/logs;
 
 #new root pw
 newrootpw=`< /dev/urandom tr -dc A-Za-z0-9 | head -c128`;
 echo root:$newrootpw | /usr/sbin/chpasswd;
 
+#new user pw
+newuserpw=`< /dev/urandom tr -dc A-Za-z0-9 | head -c24`;
+echo user:$newuserpw | /usr/sbin/chpasswd;
+echo "";
+echo "";
+echo $newuserpw;
+echo "";
+echo "";
+echo "Or make a new user password: \n";
+passwd user;
+echo "";
+echo "";
+
 #install
 apt-get -y install libappindicator1 libcurl3 libdbusmenu-glib4 libdbusmenu-gtk4 libindicator7;
-dpkg -i google-chrome-stable_current_amd64.deb;
+dpkg -i /media/user/Storage/Scripts/deb8live-init/google-chrome-stable_current_amd64.deb;
+
+echo "";echo "";echo "";echo "";echo "";echo "";echo "";
+
 apt-get -y install vim;
+echo "";echo "";echo "";echo "";echo "";echo "";echo "";
 apt-get -y install vlc;
+echo "";echo "";echo "";echo "";echo "";echo "";echo "";
 apt-get -y install git;
+echo "";echo "";echo "";echo "";echo "";echo "";echo "";
 #apt-get -y install x2goclient;
 
 #hostname
 newhostname=`< /dev/urandom tr -dc A-Za-z0-9 | head -c33`;
-sed -i 's/debian/$newhostname/g' /etc/hosts;
-sed -i 's/debian/$newhostname/g' /etc/hostname;
+sed -i "s/debian/$newhostname/g" /etc/hosts;
+sed -i "s/debian/$newhostname/g" /etc/hostname;
 invoke-rc.d hostname.sh start;
 invoke-rc.d networking force-reload;
 invoke-rc.d network-manager force-reload;
 
+echo "";echo "";echo "";echo "";echo "";echo "";echo "";
 
 
 #ConfigServer Firewall
@@ -60,13 +87,13 @@ sed -i 's/TESTING\ =\ "1"/TESTING\ =\ "0"/g' /etc/csf/csf.conf;
 sed -i 's/FASTSTART\ =\ "1"/FASTSTART\ =\ "0"/g' /etc/csf/csf.conf;
 
 #tcp in
-sed -i 's/TCP_IN = "20,21,22,25,53,80,110,143,443,465,587,993,995"/TCP_IN = "51413,9876"/g' /etc/csf/csf.conf;
+sed -i 's/TCP_IN = "20,21,22,25,53,80,110,143,443,465,587,993,995"/TCP_IN = "53,51413,9876"/g' /etc/csf/csf.conf;
 
 #tcp out
 sed -i 's/TCP_OUT = "20,21,22,25,53,80,110,113,443,587,993,995"/TCP_OUT = "22,53,80.443.5000.6969,8080.8083.51413"/g' /etc/csf/csf.conf;
 
 #udp in
-sed -i 's/UDP_IN = "20,21,53"/UDP_IN = ""/g' /etc/csf/csf.conf;
+sed -i 's/UDP_IN = "20,21,53"/UDP_IN = "53"/g' /etc/csf/csf.conf;
 
 #udp out
 sed -i 's/UDP_OUT = "20,21,53,113,123"/UDP_OUT = "53,113,123"/g' /etc/csf/csf.conf;
@@ -83,6 +110,7 @@ sed -i 's/UDP6_IN = "20,21,53"/UDP6_IN = ""/g' /etc/csf/csf.conf;
 #udp6 out
 sed -i 's/UDP6_OUT = "20,21,53,113,123"/UDP6_OUT = ""/g' /etc/csf/csf.conf;
 
+echo "";echo "";echo "";echo "";echo "";echo "";echo "";
 
 #disable, enable, reload rules
 /usr/sbin/csf -x;
@@ -90,13 +118,16 @@ sleep 1;
 /usr/sbin/csf -e;
 sleep 1;
 /usr/sbin/csf -r;
+echo "";echo "";echo "";echo "";echo "";echo "";echo "";
 
 
 
 
 updatedb;
+echo "";echo "";echo "";echo "";echo "";echo "";echo "";
 
 apt-get -y update;
+echo "";echo "";echo "";echo "";echo "";echo "";echo "";
 
 echo "";
 echo "";
--- user-second.sh
+++ user-second.sh
@@ -1,12 +1,45 @@
 #!/bin/bash
-echo "";
-echo "";
-echo "Make a new strong password: \n";
-sudo passwd user;
-echo "";
-echo "";
+#
+# Author: Art Lux
+# Copyright: Art Lux 2015
+# Info: Debian 8 live user customization script.
 
-#customzie 
+#gnome privacy
+gsettings set org.gnome.desktop.privacy hide-identity true;
+gsettings set org.gnome.desktop.privacy old-files-age 0;
+gsettings set org.gnome.desktop.privacy remember-app-usage false;
+gsettings set org.gnome.desktop.privacy remember-recent-files false;
+gsettings set org.gnome.desktop.privacy remove-old-temp-files true;
+gsettings set org.gnome.desktop.privacy remove-old-trash-files true;
+gsettings set org.gnome.desktop.privacy send-software-usage-stats false;
+
+#timezone
+gsettings set org.gnome.desktop.datetime automatic-timezone true;
+
+#clock
+gsettings set org.gnome.desktop.interface clock-format 12h;
+gsettings set org.gnome.desktop.interface clock-show-date true;
+
+#launcher
+gsettings set org.gnome.shell favoriate-apps "['google-chrome.desktop', 'gnome-terminal,desktop', 'gedit.desktop', 'nautilus.desktop', 'iceweasel.desktop', 'gnome-calculator.desktop']"
+
+#monospace
+gsettings set org.gnome.desktop.interface monospace-font-name "Monospace 9";
+
+#global dark theme
+gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark";
+
+#background
+gsettings set org.gnome.desktop.background color-shading-type "solid";
+gsettings set org.gnome.desktop.background picture-uri "";
+gsettings set org.gnome.desktop.background primary-color "#000";
+
+#cursor size
+gsettings set org.gnome.desktop.interface cursor-size 32;
+
+#screensaver lock
+gsettings set org.gnome.desktop.screensaver lock-enabled false;
+
 #gedit
 gsettings set org.gnome.gedit.preferences.editor auto-indent true;
 gsettings set org.gnome.gedit.preferences.editor display-line-numbers true;
@@ -23,62 +56,20 @@ gsettings set org.gnome.SimpleScan photo-dpi 2400;
 gsettings set org.gnome.SimpleScan page-side front;
 
 
-#timezone
-gsettings set org.gnome.desktop.datetime automatic-timezone true;
-
-#clock
-gsettings set org.gnome.desktop.interface clock-format 12h;
-gsettings set org.gnome.desktop.interface clock-show-date true;
-
-
-gsettings set org.gnome.desktop.interface cursor-size 32;
-
-gsettings set org.gnome.desktop.interface monospace-font-name "Monospace 9";
-
-gsettings set org.gnome.desktop.privacy hide-identity true;
-gsettings set org.gnome.desktop.privacy old-files-age 0;
-gsettings set org.gnome.desktop.privacy remember-app-usage false;
-gsettings set org.gnome.desktop.privacy remember-recent-files false;
-gsettings set org.gnome.desktop.privacy remove-old-temp-files true;
-gsettings set org.gnome.desktop.privacy remove-old-trash-files true;
-gsettings set org.gnome.desktop.privacy send-software-usage-stats false;
-
-
-gsettings set org.gnome.desktop.screensaver lock-enabled false;
-
-gsettings set org.gnome.desktop.background color-shading-type "solid";
-gsettings set org.gnome.desktop.background picture-uri "";
-gsettings set org.gnome.desktop.background primary-color "#000";
-
-gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark";
-
-
 #install adblock (firefox/iceweasel)
-#rm adblockplusfirefox.xpi;
-#wget https://update.adblockplus.org/latest/adblockplusfirefox.xpi;
-#firefox adblockplusfirefox.xpi
-#rm adblockplusfirefox.xpi;
 firefox https://update.adblockplus.org/latest/adblockplusfirefox.xpi;
 
 #install noscript (firefox/iceweasel)
-#rm addon-722-latest.xpi;
-#wget https://addons.mozilla.org/en-US/firefox/downloads/latest/722/addon-722-latest.xpi;
-#firefox addon-722-latest.xpi;
-#rm addon-722-latest.xpi;
 firefox https://addons.mozilla.org/en-US/firefox/downloads/latest/722/addon-722-latest.xpi;
 
-
 #adblock
-chromium https://chrome.google.com/webstore/detail/cfhdojbkjhnklbpkdaibdccddilifddb
+google-chrome https://chrome.google.com/webstore/detail/cfhdojbkjhnklbpkdaibdccddilifddb
 
+#ublock origin
+google-chrome https://chrome.google.com/webstore/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm
 #ublock
-chromium https://chrome.google.com/webstore/detail/ublock/epcnnfbjfcgphgdmggkamkmgojdagdnn
-
-#umatrix
-#https://chrome.google.com/webstore/detail/umatrix/ogfcmafjalglgifnmanfmnieipoejdcf
+#google-chrome https://chrome.google.com/webstore/detail/ublock/epcnnfbjfcgphgdmggkamkmgojdagdnn
 
 #random hide user-agent
-chromium https://chrome.google.com/webstore/detail/random-hide-user-agent/einpaelgookohagofgnnkcfjbkkgepnp
-
-
+google-chrome https://chrome.google.com/webstore/detail/random-hide-user-agent/einpaelgookohagofgnnkcfjbkkgepnp
 
